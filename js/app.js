d3e246ac9f283c5d77a9ee6067356e462fd6640b
looper44 <seba.kubacki.kun@gmail.com>
2017-09-12 16:22:47 +0200

    Some cosmetic changes, added section with plants

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
 $(document).ready( function() {
+
+	$('.choose-animals').on('click', function(){
+		$('#animals-content').removeClass('hidden')
+		$('#plants-content').addClass('hidden')
+	})
+
+	$('.choose-home').on('click', function(){
+		$('#animals-content').addClass('hidden')
+		$('#plants-content').addClass('hidden')
+	})
+
+	$('.choose-plants').on('click', function(){
+		$('#animals-content').addClass('hidden')
+		$('#plants-content').removeClass('hidden')
+	})
+
+
+
 	$('.subMenu').smint();
-  
+
   $('.subMenu').smint({
   	'scrollSpeed' : 1000
   });
+
 
 
 });
